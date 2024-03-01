667b7fbea7ce9ea6f755fbaf866abbf620ab156f
abhilashvadnala <abhivadnala@gmail.com>
2024-03-01 15:57:51 -0800

    rendering a react element

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const App = React.createElement("div", {
-    id: "root"
-}, <h1>Namaste React</h1>);
+const App = <div id="app">Namaste React with JSX</div>
 
 ReactDOM.createRoot(document.getElementById("root")).render(App);
