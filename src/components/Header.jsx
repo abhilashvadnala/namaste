b73ae91104d20bab5d1c22580339f98524c1690a
abhilashvadnala <abhivadnala@gmail.com>
2024-03-11 12:31:16 -0700

    added all

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import Search from "./Search";
-import { LOGO_2_URL } from "../utils/constnants";
+import { LOGO_URL } from "../utils/constnants";
 
 export default Header = () => {
     return (
         <div className="header">
             <div className="logo-container">
-                <img src={LOGO_2_URL} />
+                <img src={LOGO_URL} />
             </div>
             <Search />
             <nav className="nav-container">
